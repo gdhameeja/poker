5a94c529a98b8e072e4a1e96cd67ea5c14ee9180
Gaurav <gdhameeja@gmail.com>
2018-07-10 00:38:12 +0530

    added tests for handrank func

--- poker.py
+++ poker.py
@@ -5,15 +5,23 @@ def besthand(hands):
 def handrank(hand):
     """ function to return the rank of a particular hand according to poker rules """
     pass
-    
 
 
 def test_poker():
     """ tests for functions in poker program """
-    tk = "2D 2S 4H 8C AS".split()
+    tk = "3D 3S 4H 8C AS".split()
     hh = "7D 2S 4H 8C AS".split()
     sf = "6C 7C 8C 9C TC".split()
     assert besthand([tk, hh, sf]) == sf
     assert besthand([tk, hh]) == tk
     assert besthand([hh, hh]) == hh
-    return "tests pass"
+    assert besthand([hh]) == hh
+    assert besthand([sf] + 100*[hh]) == sf
+
+    # 1st 2 is ranking of the hand among all hands, 3 is the rank of the card
+    assert handrank(tk) == (2, 3)
+
+    # 1st 6 is the rank of the hand among all hands, 10 is the highest card in hand
+    assert handrank(sf) == (8, 10)
+    
+    return "OK"
